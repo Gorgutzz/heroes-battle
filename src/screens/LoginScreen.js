52134cd75bdbb4b3880ec69525db4b5adb85516f
leo <leo.kalshteyn@gmail.com>
2020-09-07 17:03:32 -0700

    added login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,1 +1,42 @@
+import React, { Component } from "react";
+import { View, TextInput, TouchableOpacity, Image } from "react-native";
 
+export default class LoginScreen extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  state = {
+    username: ""
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <View style={styles.main}>
+          <CustomText styles={styles.label}>Enter username</CustomText>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={username => this.setState({ username })}
+            value={this.state.username}
+          />
+
+          <TouchableOpacity onPress={this.login} style={styles.button}>
+            <CustomText styles={styles.buttonText}>Sign in</CustomText>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  login = () => {
+    let username = this.state.username;
+
+    if (username) {
+      this.props.navigation.navigate("HeroSelect", {
+        username
+      });
+    }
+  };
+}
