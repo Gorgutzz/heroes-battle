163402efbad0633d019a3065e64d47f7279340e6
leo <leo.kalshteyn@gmail.com>
2020-09-26 17:31:41 -0700

    added styling for login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -68,3 +68,45 @@ export default class LoginScreen extends Component {
     }
   };
 }
+
+const styles = {
+  container: {
+    flex: 10,
+    padding: 20,
+    backgroundColor: "#FFF"
+  },
+  top: {
+    flex: 4,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 10
+  },
+  main: {
+    flex: 6,
+    justifyContent: "flex-start"
+  },
+  label: {
+    fontSize: 16
+  },
+  textInput: {
+    height: 40,
+    marginTop: 5,
+    marginBottom: 10,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    backgroundColor: "#eaeaea",
+    padding: 5
+  },
+  button: {
+    alignSelf: "center",
+    marginTop: 10
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#05a5d1"
+  }
+};
