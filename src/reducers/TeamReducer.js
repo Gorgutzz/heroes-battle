1ed5a3605ab14139eadae4478cf36ed0c347fe00
leo <leo.kalshteyn@gmail.com>
2020-09-26 17:18:20 -0700

    added select hero case to team reducer

--- src/reducers/TeamReducer.js
+++ src/reducers/TeamReducer.js
@@ -6,3 +6,36 @@ const INITIAL_STATE = {
   hero: hero_data,
   selected_hero: []
 };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case SELECT_HERO:
+      let hero = [...state.hero];
+      let selected_hero = [...state.selected_hero];
+
+      const is_selected = action.is_selected;
+
+      if (state.selected_hero.length < 6 || is_selected) {
+        hero = hero.map(item => {
+          if (item.id == action.id) {
+            item.is_selected = !is_selected;
+          }
+          return item;
+        });
+
+        if (is_selected) {
+          const index_to_remove = selected_hero.findIndex(
+            item => item.id == action.id
+          );
+          selected_hero.splice(index_to_remove, 1);
+        } else {
+          selected_hero.push(action.hero_data);
+        }
+      }
+
+      return { ...state, hero, selected_hero };
+
+    default:
+      return state;
+  }
+};
