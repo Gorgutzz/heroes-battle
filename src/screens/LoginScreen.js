d000627e0af3a7770c1ec82723529ad588716742
leo <leo.kalshteyn@gmail.com>
2020-09-26 17:30:53 -0700

    Made logins screen and navigation to team select

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { View, TextInput, TouchableOpacity, Image } from "react-native";
 
+import CustomText from "../components/CustomText";
+
+import { Audio } from "expo";
+
 export default class LoginScreen extends Component {
   static navigationOptions = {
     header: null
@@ -10,9 +14,31 @@ export default class LoginScreen extends Component {
     username: ""
   };
 
+  constructor(props) {
+    super(props);
+    this.backgroundSound = null;
+  }
+
+  async componentDidMount() {
+    try {
+      this.backgroundSound = new Audio.Sound();
+      await this.backgroundSound.loadAsync(
+        require("")
+      );
+      await this.backgroundSound.setIsLoopingAsync(true);
+      await this.backgroundSound.playAsync();
+    } catch (error) {
+      console.log("error loading background sound: ", error);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.top}>
+          <Image source={require("../assets/images/heroes/captain-america.gif")} />
+          <CustomText styles={styles.headerText}>Hero Battles</CustomText>
+        </View>
 
         <View style={styles.main}>
           <CustomText styles={styles.label}>Enter username</CustomText>
@@ -34,9 +60,11 @@ export default class LoginScreen extends Component {
     let username = this.state.username;
 
     if (username) {
-      this.props.navigation.navigate("HeroSelect", {
+      this.props.navigation.navigate("TeamSelect", {
         username
       });
+
+      this.backgroundSound.stopAsync();
     }
   };
 }
