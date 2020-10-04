7a3fb67dc5d8053d6dbd704ec6b8deb72e481db8
leo <leo.kalshteyn@gmail.com>
2020-10-04 15:24:58 -0700

    added server js file and initalized packages

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var Pusher = require("pusher");
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require("dotenv").config();
+
+var users = [];
+
+var pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.APP_KEY,
+  secret: process.env.APP_SECRET,
+  cluster: process.env.APP_CLUSTER
+});
