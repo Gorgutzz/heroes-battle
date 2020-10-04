77e708c54ab0842628d4f2068fae24e1c8f2e23a
leo <leo.kalshteyn@gmail.com>
2020-10-04 15:34:42 -0700

    added route for handling authentication requests

--- server/server.js
+++ server/server.js
@@ -16,3 +16,62 @@ var pusher = new Pusher({
   secret: process.env.APP_SECRET,
   cluster: process.env.APP_CLUSTER
 });
+
+app.get("/", function(req, res) {
+  // testing
+  res.send("working");
+});
+
+app.post("/pusher/auth", function(req, res) {
+  var username = req.body.username;
+  var hero_ids = req.body.hero_ids;
+  var team_member_ids = req.body.team_member_ids;
+
+  console.log("hero ids: ", hero_ids);
+  console.log("team member ids: ", team_member_ids);
+
+  let user_index = users.findIndex(item => {
+    return item.username == username;
+  });
+
+  if (user_index === -1) {
+    users.push({
+      username: username,
+      hero_ids: hero_ids,
+      team_member_ids: team_member_ids
+    });
+
+    if (users.length == 2) {
+      var player_one_index = 0;
+      var player_one = users.splice(player_one_index, 1)[0];
+
+      var player_two_index = 0;
+      var player_two = users.splice(player_two_index, 1)[0];
+
+
+      pusher.trigger("private-user-" + player_one.username, "opponent-found", {
+        player_one: player_one,
+        player_two: player_two
+      });
+
+      setTimeout(() => {
+        pusher.trigger(
+          "private-user-" + player_two.username,
+          "opponent-found",
+          {
+            player_one: player_one,
+            player_two: player_two
+          }
+        );
+      }, 3000);
+    }
+
+    var socketId = req.body.socket_id;
+    var channel = req.body.channel_name;
+    var auth = pusher.authenticate(socketId, channel);
+
+    res.send(auth);
+  } else {
+    res.status(400);
+  }
+});
