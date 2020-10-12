98b203fadb06c6034c3e7ecaa1837f513c7ac1c6
leo <leo.kalshteyn@gmail.com>
2020-10-12 15:53:49 -0700

    added more moves

--- src/data/moves_data.js
+++ src/data/moves_data.js
@@ -2,22 +2,70 @@ const moves_data = [
   {
     id: 1,
     title: "Kick",
-    power: 40,
+    power: 30,
     type: "normal",
     is_first: true
   },
   {
     id: 2,
     title: "Punch",
-    power: 80,
+    power: 30,
     type: "normal"
   },
   {
     id: 3,
-    title: "Hadoken",
-    power: 95,
+    title: "Heat Vision",
+    power: 55,
     type: "energy"
-  }
+  },
+  {
+    id: 4,
+    title: "Inhale Capture",
+    power: 45,
+    type: "immobilize"
+  },
+  {
+    id: 5,
+    title: "Up, Up and Away",
+    power: 70,
+    type: "flying"
+  },
+  {
+    id: 6,
+    title: "Batarang",
+    power: 55,
+    type: "weapon"
+  },
+  {
+    id: 7,
+    title: "Smoke Escape",
+    power: 10,
+    type: "evade"
+  },
+  {
+    id: 8,
+    title: "Grappling Hook",
+    power: 40,
+    type: "immobilize"
+  },
+  {
+    id: 8,
+    title: "Lightsaber Attack",
+    power: 50,
+    type: "weapon"
+  },
+  {
+    id: 8,
+    title: "Force Push",
+    power: 35,
+    type: "Magic"
+  },
+  {
+    id: 8,
+    title: "Force Choke",
+    power: 75,
+    type: "Magic"
+  },
 ];
 
 export default moves_data;
